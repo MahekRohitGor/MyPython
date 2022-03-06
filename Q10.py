0022eb1f61c267d9d3ad36d7e1bff4aa3c644db6
Mahek Rohit Gor <1mahekgor1@gmail.com>
2022-03-06 15:23:24 +0530

    Python Questions

--- Q10.py
+++ Q10.py
@@ -1,7 +1,10 @@
 # Write a Python program to display the examination schedule.
-date_of_exam = str(input("Enter Date of exam in dd,mm,yyyy(should be separated by commas):"))
-x = date_of_exam.split(",")
-print("Date of examination is "+x[0]+"/"+x[1]+"/"+x[2])
+# date_of_exam = str(input("Enter Date of exam in dd,mm,yyyy(should be separated by commas):"))
+# x = date_of_exam.split(",")
+# print("Date of examination is "+x[0]+"/"+x[1]+"/"+x[2])
 # Or
-date_of_exam = (12, 4, 2027)
+day = int(input("Enter day of exam: "))
+month = int(input("Enter month of exam: "))
+year = int(input("Enter year of exam: "))
+date_of_exam = (day, month, year)
 print("Date of exam is : %i/%i/%i"%date_of_exam)
