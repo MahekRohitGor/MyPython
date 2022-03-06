0f9b6bfca98bd30f87b15e664fbded282d42bc7a
Mahek Rohit Gor <1mahekgor1@gmail.com>
2022-03-06 16:29:28 +0530

    Python Questions

---
+++ Q22.py
@@ -0,0 +1,8 @@
+# accept 3 numbers and find their average.
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+
+avg = (num1+num2+num3)/3
+
+print("Average of three is : " + str(avg))
